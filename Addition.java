cd55278e0c0df6c23bdb06d00183746b27f8b1b2
PratikshaMhaske <pratikshamhaske2004@gmail.com>
2024-07-15 20:12:44 +0530

    Addition.java

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+    public static void main(String s[])
+    {
+          int a=10;
+          int b=6;
+          int c=a+b;
+          System.out.println("Addition of a and b is="+c);
+    }
+}
